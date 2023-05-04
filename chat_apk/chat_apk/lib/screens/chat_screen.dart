6481a34069a2a524181c7377c2e5dd4ef2ae6be0
Yubraj Adhikari <yubrajadhikari987@gmail.com>
2023-05-04 22:03:19 +0545

    new screen added

--- chat_apk/chat_apk/lib/screens/chat_screen.dart
+++ chat_apk/chat_apk/lib/screens/chat_screen.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ChatApp"),
-      ),
-      body: ListView.builder(
-        itemBuilder: ((context, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text('Hello Everyone'),
-            )),
-        itemCount: 10,
-      ),
-    );
+        appBar: AppBar(
+          title: Text("ChatApp"),
+        ),
+        body: ListView.builder(
+          itemBuilder: ((context, index) => Container(
+                padding: EdgeInsets.all(8),
+                child: Text('Hello Everyone'),
+              )),
+          itemCount: 10,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (() {
+            final firestore = FirebaseFirestore.instance;
+            firestore
+                .collection('messages/F25hXek1gAJAfhCMwvnS/chat')
+                .snapshots()
+                .listen((data) {
+              print(data);
+            }); 
+          }),
+        ));
   }
 }
